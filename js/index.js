bb5aa5dc4ca60bcf6b8f83c010307cb72471a135
Wellsjohnny <wmsswizard@gmail.com>
2020-06-05 22:15:38 -0300

    TrocaTelaComButton

---
+++ js/index.js
@@ -0,0 +1,12 @@
+document.getElementById("myButton").onclick = function () {
+    location.href = "pagina2.html";
+};
+
+document.getElementById("btnpag2").onclick = function () {
+    location.href = "pagina3.html";
+};
+
+document.getElementById("btn1").onclick = function () {
+    location.href = "index.html";
+};
+
